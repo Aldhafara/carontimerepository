1786809d8e7411d59a3797a12fb91f311d321e74
MichalMarchwinski <michal.marchwinski@gmail.com>
2018-12-01 15:19:07 +0100

    Added mapapi.html - with map included

--- src/main/java/com/carontime/on_time/controler/CarOnTimeControler.java
+++ src/main/java/com/carontime/on_time/controler/CarOnTimeControler.java
@@ -1,4 +1,33 @@
 package com.carontime.on_time.controler;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class CarOnTimeControler {
+
+    @GetMapping("/")
+    public String start() {
+        return "layout";
+    }
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "userData";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "adminData";
+    }
+
+
+
 }
