a54045e67e6a69bc178581626569573b39c8bb08
Aldhafara <aldhafara@onet.pl>
2018-12-02 13:02:27 +0100

    Add userService implementation

--- src/main/java/com/carontime/on_time/model/user/User.java
+++ src/main/java/com/carontime/on_time/model/user/User.java
@@ -16,5 +16,73 @@ public class User {
     private String carLicenceId;
     private String emailAdress;
     private int phoneNumber;
+    private int wallet = 0;
 
+    public User() {}
+
+    public User(String name, String lastname, String city, String carLicenceId, String emailAdress, int phoneNumber, int wallet) {
+        this.name = name;
+        this.lastname = lastname;
+        this.city = city;
+        this.carLicenceId = carLicenceId;
+        this.emailAdress = emailAdress;
+        this.phoneNumber = phoneNumber;
+        this.wallet = wallet;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCarLicenceId() {
+        return carLicenceId;
+    }
+
+    public void setCarLicenceId(String carLicenceId) {
+        this.carLicenceId = carLicenceId;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
---
+++ src/main/java/com/carontime/on_time/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.carontime.on_time.service;
+
+import com.carontime.on_time.model.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.carontime.on_time.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements userService{
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+}
