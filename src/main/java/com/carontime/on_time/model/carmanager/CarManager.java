90397920189e21cde94509675177580fddc7999f
AxL <rafal.zoltanski@gmail.com>
2018-12-02 09:55:43 +0100

    Przeniesienie i zmiana nazwy CarManager -> CarRepository

--- src/main/java/com/carontime/on_time/model/carmanager/CarManager.java
+++
@@ -1,8 +0,0 @@
-package com.carontime.on_time.model.carmanager;
-
-import com.carontime.on_time.model.car.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarManager extends CrudRepository<Car, Integer> {
-
-}
