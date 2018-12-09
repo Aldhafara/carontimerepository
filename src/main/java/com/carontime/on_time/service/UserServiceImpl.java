e633d5d6ec9b6589d39b1e9ec17b558fbe559282
AxL <rafal.zoltanski@gmail.com>
2018-12-09 12:52:19 +0100

    dodano użytkownika

--- src/main/java/com/carontime/on_time/service/UserServiceImpl.java
+++ src/main/java/com/carontime/on_time/service/UserServiceImpl.java
@@ -49,9 +49,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByEmailAdress(email).orElse(null);
     }
 
-
     @Override
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
     }
 }
